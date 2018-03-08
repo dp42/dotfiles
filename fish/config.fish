eb94260be7969a5f701015b3bf44946c275f395a
Koichi Sato <carryatowel42@gmail.com>
2018-03-08 20:04:43 +0900

    update

--- fish/config.fish
+++ fish/config.fish
@@ -4,11 +4,14 @@ set -gx EDITOR vim
 # Set paths
 rbenv init - | source
 set -gx GOPATH $HOME/Dev
+set -gx PATH $HOME/bin $PATH
 set -gx PATH $GOPATH/bin $PATH
 set -gx PATH $HOME/.nodebrew/current/bin $PATH
 set -gx PATH $HOME/.composer/vendor/bin $PATH
 set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
 set -gx MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+
+alias diary='vim ~/Diary/(date +%Y/diary-%Y-%m-%d.md)'
 
 function fish_user_key_bindings
     bind \cs peco_change_repository
