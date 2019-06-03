9610889a293ee94903135d816407edbe00f03f10
Koichi SATO <carryatowel42@gmail.com>
2019-06-03 18:10:19 +0900

    update

--- fish/functions/gpf.fish
+++ fish/functions/gpf.fish
@@ -2,7 +2,7 @@ function gpf
   set branch (git rev-parse --abbrev-ref HEAD)
 
   if [ $branch != 'master' ]
-    git push --force origin $branch
+    git push --force-with-lease origin $branch
   else
     echo 'Force Push to Master is prohibited!'
   end
