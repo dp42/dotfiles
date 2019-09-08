71c442db10fbc26c7d63e6c9f72239e3974f107e
Koichi SATO <carryatowel42@gmail.com>
2019-09-08 23:41:43 +0900

    update

--- init.sh
+++ init.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-ln -nfs ~/src/github.com/dp42/dotfiles/alacritty $HOME/.config/alacritty
-ln -nfs ~/src/github.com/dp42/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -nfs ~/src/github.com/dp42/dotfiles/fish $HOME/.config/fish
+ln -nfs ~/src/github.com/dp42/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -nfs ~/src/github.com/dp42/dotfiles/.vim $HOME/.vim
-ln -nfs ~/src/github.com/dp42/dotfiles/peco $HOME/.config/peco
 ln -nfs ~/src/github.com/dp42/dotfiles/.tigrc $HOME/.tigrc
 ln -nfs ~/src/github.com/dp42/dotfiles/.editorconfig $HOME/.editorconfig
 ln -nfs ~/src/github.com/dp42/dotfiles/.dircolors $HOME/.dircolors
 ln -nfs ~/src/github.com/dp42/dotfiles/.ctags.d $HOME/.ctags.d
 ln -nfs ~/src/github.com/dp42/dotfiles/git $HOME/.config/git
 ln -nfs ~/src/github.com/dp42/dotfiles/coc $HOME/.config/coc
+ln -nfs ~/src/github.com/dp42/dotfiles/starship.toml $HOME/.config/starship.toml
