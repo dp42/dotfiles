e9b1813ca0d9d24a94543bc0357fd49678d73f6c
Koichi SATO <carryatowel42@gmail.com>
2020-03-28 23:46:36 +0900

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,15 @@
+start_tmux
+
+# ENV
 set -gx SHELL /usr/local/bin/fish
 set -gx EDITOR vim
 set -x LANG ja_JP.UTF-8
 set -x LC_ALL ja_JP.UTF-8
+set -x FZF_DEFAULT_COMMAND 'rg --files -g !.git/ --hidden'
+set -x FZF_DEFAULT_OPTS '--height 30% --reverse'
+set -x BAT_CONFIG_PATH $HOME/.config/bat/bat.conf
 
 # PATH
-set -gx PHPBREW_RC_ENABLE 1
-set -gx PATH /usr/local/bin $PATH
 rbenv init - | source
 source $HOME/.phpbrew/phpbrew.fish
 set -gx GOPATH $HOME
@@ -16,33 +20,22 @@ set -gx PATH $HOME/.phpbrew/bin $PATH
 set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
 set -gx PATH /usr/local/opt/python/libexec/bin $PATH
 set -gx PATH /usr/local/opt/llvm/bin $PATH
-set -gx PATH $HOME/.cargo/bin  $PATH
 set -gx PATH /usr/local/opt/mysql@5.7/bin $PATH
 set -gx MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
-set -gx FZF_DEFAULT_COMMAND 'rg --files -g !.git/ --hidden'
-set -gx FZF_DEFAULT_OPTS '--height 30% --reverse'
-set -gx BAT_CONFIG_PATH $HOME/.config/bat/bat.conf
+set -gx PHPBREW_RC_ENABLE 1
 
-#Alias
+# Alias
 alias gpl='git pull origin (git rev-parse --abbrev-ref HEAD)'
 alias gps='git push origin (git rev-parse --abbrev-ref HEAD)'
-
-# bobthefish
-set -g theme_display_git_master_branch yes
 
 # powerline
 set fish_function_path $fish_function_path "/usr/local/lib/python3.7/site-packages/powerline/bindings/fish"
 powerline-setup
-
 
 function fish_user_key_bindings
 bind \cs fzf_change_repository
 bind \cr fzf_history
 end
 
-eval (direnv hook fish)
 eval (starship init fish)
-start_tmux
-set -g fish_user_paths "/usr/local/opt/mysql@5.7/bin" $fish_user_paths
 direnv hook fish | source
-
